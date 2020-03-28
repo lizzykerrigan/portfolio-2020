1b562d6b1d441de923e8e0682f5d9e5c2c5e398b
lizzykerrigan <lizzykerrigan@outlook.com>
2020-03-28 13:47:46 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-intro">
+          <div className="section">
+            <h1>Elizabeth Kerrigan</h1>
+            <span>British, b. 1991</span>
+          </div>
+          <div className="section">
+            <h2>Portfolio Site, 2020</h2>
+            <span>TypeScript on React</span>
+          </div>
+          <div className="section">
+            <span>Private collection</span>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
